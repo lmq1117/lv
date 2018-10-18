1991d698d564782d36c7c4f079a8d69f390b9eb5
limq <limq@cgnfs.com.cn>
2018-10-18 11:17:08 +0800

    Log

--- app/Http/Controllers/GitWebhookController.php
+++ app/Http/Controllers/GitWebhookController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class GitWebhookController extends Controller
 {
     //
     public function pull(Request $request){
         $secret = 'laravelvuegit';
-        dd($request->all());
+        //dd($request->all());
+        Log::info(json_encode($request->all()));
     }
 }
